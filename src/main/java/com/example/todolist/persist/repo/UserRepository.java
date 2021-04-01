cb99527365311acf4800a0f7047cc9635d45a116
skvizhinadze <skvizhinadze@softclub-sx24.ru>
2021-04-01 23:14:19 +0300

    create initial structure

---
+++ src/main/java/com/example/todolist/persist/repo/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.todolist.persist.repo;
+
+import com.example.todolist.persist.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
